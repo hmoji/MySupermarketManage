44c4f07d0bcb232a65935d2aa45c28f9b426f98a
hmoji <945619928@qq.com>
2020-03-04 23:00:07 +0800

    第一次提交，完成基本配置

---
+++ src/main/java/smmanage/controller/Controllertest.java
@@ -0,0 +1,12 @@
+package smmanage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controllertest {
+	@RequestMapping("jumpToWelcome")
+	public String controllerString() {
+	return "welcome";
+}
+}
